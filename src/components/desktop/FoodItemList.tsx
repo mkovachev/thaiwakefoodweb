4e497fa76d322ef675dece424aa22673f00703a5
Martin Kovachev <m.kovachev@mitivate.co>
2023-02-22 11:28:00 +0700

    added image modal

--- src/components/desktop/FoodItemList.tsx
+++ src/components/desktop/FoodItemList.tsx
@@ -2,8 +2,9 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, Grid, styled } from '@mui/material';
+import { Fade, Grid, Modal, styled } from '@mui/material';
 import { FoodItem } from '../../data';
+import { useState } from 'react';
 
 export interface FoodItemListProps {
   foodItems: FoodItem[]
@@ -11,42 +12,64 @@ export interface FoodItemListProps {
 
 
 export const FoodItemList = ({ foodItems }: FoodItemListProps) => {
+  const [open, setOpen] = useState(false)
+  const [image, setImage] = useState('')
+  const [title, setTitle] = useState('')
 
-  const openCardDetails = () => {
-    console.log('added to favorites')
+  const openCardDetails = (image: string, title: string) => {
+    setOpen(true)
+    setImage(image)
+    setTitle(title)
+  }
+
+  const handleClose = () => {
+    setOpen(false)
   }
 
   return (
-    <GridContainer container gap={1}>
+    <Grid container gap={1}>
       {foodItems?.map((foodItem: FoodItem, index) =>
-        <GridItem item key={index}>
-          <CardActionArea sx={{ height: '100%' }} onClick={openCardDetails}>
-            <CardStyled>
-              <CardMediaStyled
-                image={foodItem.image}
-                title={foodItem.title}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {foodItem.title}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  {foodItem.description}
-                </Typography>
-              </CardContent>
-              {/* <CardActions>
-                <Button onClick={openCardDetails} startIcon={<FavoriteBorderOutlinedIcon />} />
-              </CardActions> */}
-            </CardStyled>
-          </CardActionArea>
+        <GridItem
+          item
+          key={index}
+          onClick={() => foodItem.image && openCardDetails(foodItem.image, foodItem.title)}
+        >
+          <CardStyled elevation={1}>
+            <CardMediaStyled
+              image={foodItem.image}
+              title={foodItem.title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                #{foodItem.id} {foodItem.title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {foodItem.description}
+              </Typography>
+            </CardContent>
+          </CardStyled>
         </GridItem>
       )}
-    </GridContainer>
+      <ModalStyled
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+      >
+        <Fade in={open} timeout={500}>
+          <img
+            src={image}
+            alt={title}
+          />
+        </Fade>
+      </ModalStyled>
+    </Grid>
   )
 }
 
-const GridContainer = styled(Grid)(({ theme }) => ({
-
+const ModalStyled = styled(Modal)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }))
 
 const GridItem = styled(Grid)(({ theme }) => ({
